e1a00edafa779d1eb8b91b42c9a3e4576f0aa471
james <james.min@travelFlan.com>
2021-09-22 21:02:41 +0900

    add simple animation on render

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+import './App.css'
+
+const App= (): ReactElement => {
+  return (
+    <div className="title">
+      hello world
+    </div>
+  )
+}
+
+export default App
