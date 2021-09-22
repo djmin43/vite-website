6ac6157036e1ce2f93ef353d9c6c03c1a0f64027
james <james.min@travelFlan.com>
2021-09-22 21:14:42 +0900

    conditional rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import './App.css'
 
 const App= (): ReactElement => {
+
+  const [isButtonActive, setIsButtonActive] = useState(false)
+
+  const toggleButton = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault()
+    setIsButtonActive(!isButtonActive)
+  }
+
   return (
-    <div className="title">
-      hello world
+    <div>
+      {isButtonActive && 
+      <p className="title">
+        hello world
+      </p>
+      }
+      <button className="button" onClick={toggleButton}>
+        toggle
+      </button>
     </div>
   )
 }
