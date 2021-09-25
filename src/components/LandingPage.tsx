d66e53298af0940ae9757c52525b7644be6dd4eb
jay <dj.min43@gmail.com>
2021-09-25 13:45:00 +0900

    playing with animation

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -5,10 +5,12 @@ import { jsx, css, keyframes } from '@emotion/react'
 const bounce = keyframes`
   from, 20%, 53%, 80%, to {
     transform: translate3d(0,0,0);
+    opacity: 0.5;
   }
 
   40%, 43% {
     transform: translate3d(0, -30px, 0);
+    opacity: 1;
   }
 
   70% {
@@ -22,6 +24,7 @@ const bounce = keyframes`
 
 const hotpink = css({
   color: 'hotpink',
+  fontSize: `100px`,
   animation: `${bounce} 1s ease infinite`
 })
 
