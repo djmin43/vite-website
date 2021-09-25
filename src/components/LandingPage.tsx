e0023719194e251582fb04a7c4d44b4035f99959
jay <dj.min43@gmail.com>
2021-09-25 13:40:58 +0900

    try keyframe for emotion

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,9 +1,28 @@
 /** @jsx jsx */
 import React, { ReactElement } from 'react'
-import { jsx, css } from '@emotion/react'
+import { jsx, css, keyframes } from '@emotion/react'
+
+const bounce = keyframes`
+  from, 20%, 53%, 80%, to {
+    transform: translate3d(0,0,0);
+  }
+
+  40%, 43% {
+    transform: translate3d(0, -30px, 0);
+  }
+
+  70% {
+    transform: translate3d(0, -15px, 0);
+  }
+
+  90% {
+    transform: translate3d(0,-4px,0);
+  }
+`
 
 const hotpink = css({
-  color: 'hotpink'
+  color: 'hotpink',
+  animation: `${bounce} 1s ease infinite`
 })
 
 const LandingPage = (): ReactElement => {
